d1958606ffef4d4cf2fb453c5caf89d5ff5eeeb4
Beretta350 <beretta.gabrielpp@gmail.com>
2023-11-20 19:02:18 -0300

    Test cicd job and run-tests fix

--- run-tests.sh
+++ run-tests.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-
 newDirectory="./tests/coverage"
-if [ ! -d "$newDirectory" ]; then
-    mkdir -p "$newDirectory"
-fi
+
+# Create the coverage directory if it doesn't exist
+mkdir -p "$newDirectory"
 
 go test github.com/tweedledo/tests/integration
 go test github.com/tweedledo/tests/unit/domain -coverpkg="github.com/tweedledo/core/domain" -coverprofile="./tests/coverage/coverage-domain.out"
 go test github.com/tweedledo/tests/unit/service -coverpkg="github.com/tweedledo/core/service" -coverprofile="./tests/coverage/coverage-service.out"
 
-sourceFile="./tests/coverage/coverage-domain.out"
-appendFile="./tests/coverage/coverage-service.out"
-thirdFile="./tests/coverage/coverage.out"
+coverageDomainFile="$newDirectory/coverage-domain.out"
+coverageServiceFile="$newDirectory/coverage-service.out"
+combinedCoverageFile="$newDirectory/coverage.out"
 
 # Copy lines from line 2 to the end of the source file and save to a temporary file
-tail -n +2 "$sourceFile" > temp.txt
-tail -n +2 "$appendFile" > temp2.txt
+tail -n +2 "$coverageDomainFile" > "$newDirectory/temp.txt"
+tail -n +2 "$coverageServiceFile" > "$newDirectory/temp2.txt"
+cat "$newDirectory/temp2.txt" >> "$newDirectory/temp.txt"
+mv "$newDirectory/temp.txt" "$combinedCoverageFile"
 
-# Append the content of the append file to the temporary file
-cat temp2.txt >> temp.txt
+echo "Complete coverage in \"$combinedCoverageFile\" file"
 
-# Rename the temporary file to the third file
-mv temp.txt "$thirdFile"
+# Generate and display an HTML coverage report
+go tool cover -html="$combinedCoverageFile"
 
-echo "Complete coverage in \"$thirdFile\" file"
-
-go tool cover -html="$thirdFile"
-
-rm temp2.txt
+# Clean up temporary files
+rm "$newDirectory/temp2.txt"
